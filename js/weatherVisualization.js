3b0f71a58106b329d3e41ef5799439609db0159f
Gymnastictoast <noahhuntisaak@gmail.com>
2016-10-22 15:35:38 -0400

    added styling and bg pattern

--- js/weatherVisualization.js
+++ js/weatherVisualization.js
@@ -6,36 +6,38 @@ var cloud;
 var canvas;
 
 function setup() {
-  canvas = createCanvas(600, 600);
-  canvas.position(500,0);
-  cloud = loadImage("images/cloud.png");
-  input = createInput();
-  loadJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2ffe122d91a7b2a3048f838649fbf8eb&units=imperial", gotData, "jsonp");
+    canvas = createCanvas(windowWidth - 400, windowHeight - 50);
+    canvas.position(350, 100);
+    cloud = loadImage("images/cloud.png");
+    input = createInput();
+    input.position(windowWidth - 920, windowHeight - 540);
+    loadJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2ffe122d91a7b2a3048f838649fbf8eb&units=imperial", gotData, "jsonp");
 
-  for(var i = 0; i < 100; i++){
-  raindrops[i] = new raindrop();
-  }
+    for (var i = 0; i < 100; i++) {
+        raindrops[i] = new raindrop();
+    }
 }
 
-function raindrop(){
-  this.x = random(width);
-  this.y = random(height);
-  this.speed = random(2);
-  this.show = function(){
-    fill(0,50,200);
-    ellipse(this.x, this.y, 6, 6);
-  },
-  this.fall = function(){
-    this.y += this.speed;
-  },
-  this.reset = function(){
-    if(this.y >= height){
-      this.y = 0;
-  }
+function raindrop() {
+    this.x = random(width);
+    this.y = random(height);
+    this.speed = random(2);
+    this.show = function() {
+            fill(0, 50, 200);
+            ellipse(this.x, this.y, 6, 6);
+        },
+        this.fall = function() {
+            this.y += this.speed;
+        },
+        this.reset = function() {
+            if (this.y >= height) {
+                this.y = 0;
+            }
+        }
 }
-}
+
 function gotData(data) {
-  weatherData = data;
+    weatherData = data;
 }
 
 
@@ -43,43 +45,44 @@ function draw() {
 
 
 
-  if (weatherData) {
-    input.changed(newCity);
+    if (weatherData) {
+        input.changed(newCity);
 
-    var grassCol = map(weatherData.main.temp, 0, 100, 130,200);
-    var r = map(weatherData.main.temp, 0, 100, 0, 255);
-    var b = map(weatherData.main.temp, 0, 100, 255, 0);
+        var grassCol = map(weatherData.main.temp, 0, 100, 130, 200);
+        var r = map(weatherData.main.temp, 0, 100, 0, 255);
+        var b = map(weatherData.main.temp, 0, 100, 255, 0);
 
-    background(r, 150, b);
+        background(r, 150, b);
 
-    if(weatherData.weather[0].main == "Clouds"){
-      image(cloud, 70,20);
-      image(cloud, 220,180);
+        if (weatherData.weather[0].main == "Clouds") {
+            image(cloud, 70, 20);
+            image(cloud, 220, 180);
+        }
+
+        if (weatherData.weather[0].main == "Clear") {
+            fill(255, 255, 10);
+            ellipse(450, 100, 130, 130);
+        }
+
+        if (weatherData.weather[0].main == "Rain") {
+            for (var i = 0; i < raindrops.length; i++) {
+                raindrops[i].show();
+                raindrops[i].fall();
+                raindrops[i].reset();
+            }
+        }
+
+        fill(100, grassCol, 100);
+        rect(0, height - 130, width, 130);
+        textSize(50);
+        textAlign(CENTER);
+        strokeText("It is currently " + weatherData.main.temp, width / 2, height / 2 - 50, 0, 255);
+        strokeText(" degrees in " + city, width / 2, height / 2 + 50, 0, 255);
     }
-
-    if(weatherData.weather[0].main == "Clear"){
-      fill(255,255,10);
-      ellipse(450,100,130,130);
-    }
-
-    if(weatherData.weather[0].main == "Rain"){
-    for(var i = 0; i < raindrops.length; i++){
-    raindrops[i].show();
-    raindrops[i].fall();
-    raindrops[i].reset();
-    }
-  }
-
-    fill(100,grassCol,100);
-    rect(0,height-130,width,130);
-    textSize(50);
-    strokeText("It is currently " + weatherData.main.temp, 0, height / 2 - 50, 0, 255);
-    strokeText(" degrees in " + city, 0, height / 2 + 50, 0, 255);
-  }
 }
 
 function newCity() {
-  city = input.value();
-  loadJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2ffe122d91a7b2a3048f838649fbf8eb&units=imperial", gotData, "jsonp");
+    city = input.value();
+    loadJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=2ffe122d91a7b2a3048f838649fbf8eb&units=imperial", gotData, "jsonp");
 
 }
